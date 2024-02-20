68918db969a2c78d78d04e6ed0f758754374ae22
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2024-02-20 10:26:28 -0300

    ProjectListItem done

--- src/components/patterns/projects/ProjectListItem/ProjectListItem.stories.tsx
+++ src/components/patterns/projects/ProjectListItem/ProjectListItem.stories.tsx
@@ -6,10 +6,38 @@ export default {
   component: ProjectListItem,
   tags: ['autodocs'],
   argTypes: {
+    projectImageUrl: {
+      description: 'Defines the URL of the image of the project in a string format.',
+      control: 'text',
+    },
+    projectName: {
+      description: 'Defines the name of the project in a string format.',
+      control: 'text',
+    },
+    projectDescription: {
+      description: 'Defines a description to the project in a string format.',
+      control: 'text',
+    },
+    projectTechUrls: {
+      description: 'Defines a string array containing the URLs of the technology images used in the project.',
+      control: 'text',
+    },
   },
 }
 
 /**This is the default version of **ProjectListItem** component:*/
 export const Default = {
-  args: {},
+  args: {
+    projectImageUrl: '/images/cutted-min/nike-desktop-shoot-cutted.png',
+    projectName: 'Nike Store',
+    projectDescription: 'Project inspired in The Official Nike Store. Fully responsive. Developed in React, NextJS, and TailwindCSS.',
+    projectTechUrls: [
+      { id: 1, name: 'HTML', url: '/svgs/html-logo.svg'},
+      { id: 2, name: 'CSS', url: '/svgs/css-logo.svg'},
+      { id: 3, name: 'JavaScript', url: '/svgs/js-logo.svg'},
+      { id: 4, name: 'React', url: '/svgs/react-logo.svg'},
+      { id: 5, name: 'NextJS', url: '/svgs/next-logo.svg'},
+      { id: 6, name: 'TailwindCSS', url: '/svgs/tailwindcss-logo.svg'},
+    ]
+  },
 }
--- src/components/patterns/projects/ProjectListItem/ProjectListItem.tsx
+++ src/components/patterns/projects/ProjectListItem/ProjectListItem.tsx
@@ -1,49 +1,58 @@
 import '/src/index.css';
 import Headline2 from '../../../atoms/typography/Headline2';
 import BodyText from '../../../atoms/typography/BodyText';
-import HtmlLogo from '../../../atoms/logos/tech_logos/HtmlLogo';
-import CssLogo from '../../../atoms/logos/tech_logos/CssLogo';
-import JSLogo from '../../../atoms/logos/tech_logos/JSLogo';
-import ReactLogo from '../../../atoms/logos/tech_logos/ReactLogo';
-import NextLogo from '../../../atoms/logos/tech_logos/NextLogo';
-import TailwindLogo from '../../../atoms/logos/tech_logos/TailwindLogo';
 
-const ProjectListItem = () => {
+interface Tech {
+  id: number;
+  name: string;
+  url: string;
+}
+
+type ProjectItemProps = {
+  projectImageUrl: string;
+  projectName: string;
+  projectDescription: string;
+  projectTechUrls: Tech[];
+}
+
+const ProjectListItem = ({ projectImageUrl, projectName, projectDescription, projectTechUrls }: ProjectItemProps) => {
   return (
     // Wrapper 
     <div className='flex justify-center items-center w-full lg:max-w-[460px] xl:max-w-[520px]'>
       {/* Container  */}
       <div className='flex flex-col gap-3'>
+
         {/* Image */}
         <div className='w-full h-[240px] sm:h-[480px] lg:h-[540px] overflow-hidden rounded shadow-custom'>
           <img 
-            src="/images/cutted-min/nike-desktop-shoot-cutted.png"
+            src={projectImageUrl}
             className='object-cover w-full h-auto'
             alt="Reference image" 
           />
         </div>
+
         {/* Texts */}
         <div className='flex flex-col gap-2'>
-          <Headline2 color='text-neutral-900'>Nike Store</Headline2>
+          <Headline2 color='text-neutral-900'>{projectName}</Headline2>
           <BodyText color='text-neutral-800'>
-            Project inspired in The Official Nike Store. Fully responsive. Developed in React, NextJS, and TailwindCSS.
+            {projectDescription}
           </BodyText>
         </div>
+
         {/* Thumbs */}
         <div className='flex gap-2 items-center'>
-          <HtmlLogo width='w-[14px] sm:w-[18px]' height='h-[14px] sm:h-[18px]'/>
-          <CssLogo width='w-[14px] sm:w-[18px]' height='h-[14px] sm:h-[18px]'/>
-          <JSLogo width='w-[14px] sm:w-[18px]' height='h-[14px] sm:h-[18px]'/>
-          <ReactLogo width='w-[14px] sm:w-[18px]' height='h-[14px] sm:h-[18px]'/>
-          <NextLogo width='w-[14px] sm:w-[18px]' height='h-[14px] sm:h-[18px]'/>
-          <TailwindLogo width='w-[14px] sm:w-[18px]' height='h-[14px] sm:h-[18px]'/>
+          {projectTechUrls.map((tech) => (
+            <img key={tech.id} src={tech.url} alt={tech.name} />
+          ))}
         </div>
+
       </div>  
     </div>
   )
 }
 
 export default ProjectListItem;
+
 
 
 
